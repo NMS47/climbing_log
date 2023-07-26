459d6fc60097caf634b705470231e8dff6faff78
NMS47 <nicolas.salvadores93@gmail.com>
2023-07-26 14:41:57 -0500

    corrected responsive problems, added a link to login for signup, chnaged signup design

--- climb_log_webapp/forms.py
+++ climb_log_webapp/forms.py
@@ -8,6 +8,11 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for myField in self.fields:
+            self.fields[myField].widget.attrs['class'] = 'form-control'
 
 
 class NewEntryForm(forms.ModelForm):
