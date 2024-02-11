d16766876f7e22ed7ba280f5461577db4babb691
NMS47 <nicolas.salvadores93@gmail.com>
2024-02-11 11:01:00 -0300

    changing degug true to fix problems

--- climbing_log/settings.py
+++ climbing_log/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("IS_DEVELOPMENT") == 'TRUE'
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = [
