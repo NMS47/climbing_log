94044a8dee87fb2681308a95922e5a09f28d0007
NMS47 <nicolas.salvadores93@gmail.com>
2024-02-11 11:16:38 -0300

    changing debug to false

--- climbing_log/settings.py
+++ climbing_log/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("IS_DEVELOPMENT") == 'TRUE'
-DEBUG = True
+DEBUG = os.getenv("IS_DEVELOPMENT") == 'TRUE'
+
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = [
